bb086ab69ca1041b7860c39cdbf80baef4fea33f
ChthollyWn <2010768120@qq.com>
2022-09-13 20:21:47 +0800

    02
    Signed-off-by: duoluogong <1950790043@qq.com>

---
+++ src/main/java/com/example/accountbook/mapper/TestdbMapper.java
@@ -0,0 +1,10 @@
+package com.example.accountbook.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.accountbook.model.Testdb;
+
+public interface TestdbMapper extends BaseMapper<Testdb> {
+
+
+
+}
